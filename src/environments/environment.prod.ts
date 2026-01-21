576345c7a8488f79ce46ba30448f68dd374e81f7
Ram <ram.rai@pinnacledigital.in>
2026-01-21 14:19:50 +0530

    added

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://infosys-backend-pr1k.onrender.com'
-};
-
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://infosys-backend-pr1k.onrender.com'
-};
-
-
